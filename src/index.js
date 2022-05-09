111db1d6c217e5b6e2b08bb43bef3caf70dedd43
Wex2 <gatsqp@gmail.com>
2022-05-09 23:58:08 +0300

    feat: add dom structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,17 @@
 import './style/style.css';
-import keyData from './keyData.json'
+import createDomElement from './createDomElement';
+import Keyboard from './Keyboard';
+
+const TITLE = 'Виртуальная клавиатура';
+const DESCRIPTION = 'Клавиатура создана в операционной системе Windows';
+const LANGUAGE = 'Для переключения языка комбинация: ctrl + alt';
+const keyboard = new Keyboard();
+
+const container = createDomElement('div', '', 'container');
+container.append(createDomElement('h1', TITLE, 'title'));
+container.append(createDomElement('textarea', '', 'textarea'));
+container.append(keyboard.createKeyboard());
+container.append(createDomElement('p', DESCRIPTION, 'description'));
+container.append(createDomElement('p', LANGUAGE, 'language'));
+document.body.append(container);
+
